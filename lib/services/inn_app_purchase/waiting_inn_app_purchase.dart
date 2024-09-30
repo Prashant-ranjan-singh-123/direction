d13c3107f9cb73eb11e4ebae95bd00d679b70e57
prashant <prashant.singh.12312345@gmail.com>
2024-09-30 10:55:01 +0530

    In-app purchase

--- lib/services/inn_app_purchase/waiting_inn_app_purchase.dart
+++ lib/services/inn_app_purchase/waiting_inn_app_purchase.dart
@@ -7,21 +7,18 @@ import '../../shared/circular_progress_indicator.dart';
 class WaitingScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return WillPopScope(
-      onWillPop: () async => false,
-      child: Scaffold(
-        body: Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              SizedBox(
-                  width: MediaQuery.of(context).size.width*0.7,
-                  child: SvgPicture.asset(AppAssets.svg_loading)),
-              AdaptiveCircularProgressIndicator(),
-              SizedBox(height: 20),
-              Text('Processing your purchase...', style: TextStyle(fontSize: 18)),
-            ],
-          ),
+    return Scaffold(
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            SizedBox(
+                width: MediaQuery.of(context).size.width*0.7,
+                child: SvgPicture.asset(AppAssets.svg_loading)),
+            AdaptiveCircularProgressIndicator(),
+            SizedBox(height: 20),
+            Text('Processing your purchase...', style: TextStyle(fontSize: 18)),
+          ],
         ),
       ),
     );
